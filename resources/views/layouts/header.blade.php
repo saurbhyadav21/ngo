29c7867f9626063962103c98083c205f49848623
saurbhyadav21 <saurbhyadav@gmail.com>
2025-06-20 16:07:19 +0530

    eee

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,7 +16,9 @@
         href='https://cdn-uicons.flaticon.com/3.0.0/uicons-bold-straight/css/uicons-bold-straight.css'>
     <link rel='stylesheet'
         href='https://cdn-uicons.flaticon.com/3.0.0/uicons-solid-straight/css/uicons-solid-straight.css'>
-    <link rel="stylesheet" href="http://127.0.0.1:8000/template/color.css">
+    <!-- <link rel="stylesheet" href="http://127.0.0.1:8000/template/color.css"> -->
+    <link rel="stylesheet" href="{{ asset('template/color.css') }}">
+
     <link rel="stylesheet" href="http://127.0.0.1:8000/template/style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
