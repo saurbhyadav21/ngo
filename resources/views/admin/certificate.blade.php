d1137aad1d3c5a3579ad1b9a1324706892a5c254
saurbhyadav21 <saurbhyadav@gmail.com>
2025-07-01 16:31:32 +0530

    certificate delete

--- resources/views/admin/certificate.blade.php
+++ resources/views/admin/certificate.blade.php
@@ -124,22 +124,28 @@ div.dataTables_wrapper {
         });
     });
 
-     $(document).on('click', '.delete', function() {
+   $(document).on('click', '.delete', function () {
     var id = $(this).data("id");
 
-    if (confirm("Are you sure you want to delete this post?")) {
+    if (confirm("Are you sure you want to delete this certificate?")) {
+        // Build the URL from Laravel’s named route (placeholder __ID__)
+        var routeTemplate = "{{ route('deleteCertificate', ['id' => '__ID__']) }}";
+        var deleteUrl     = routeTemplate.replace('__ID__', id);
+
         $.ajax({
-            url: "/deleteCertificate/" + id,
-            type: "DELETE",
+            url:  deleteUrl,
+            type: "POST",                 // POST + method spoofing
             data: {
-                _token: "{{ csrf_token() }}"
+                _token: "{{ csrf_token() }}",
+                _method: "DELETE"
             },
-            success: function(response) {
-                alert(response.success);
-                $('#users-table').DataTable().ajax.reload(); // reload datatable
+            success: function (response) {
+                alert(response.success || "Certificate deleted successfully.");
+                $('#users-table').DataTable().ajax.reload(); // reload DataTable
             },
-            error: function(xhr) {
-                alert('Error deleting post.');
+            error: function (xhr) {
+                console.error(xhr.responseText);
+                alert("Error deleting certificate.");
             }
         });
     }
