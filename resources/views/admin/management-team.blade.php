e116777f441392c47e86782dd049eabe2a8d5671
saurbhyadav21 <saurbhyadav@gmail.com>
2025-07-01 14:51:50 +0530

    d

--- resources/views/admin/management-team.blade.php
+++ resources/views/admin/management-team.blade.php
@@ -125,7 +125,7 @@ div.dataTables_wrapper {
   $(document).on('click', '.delete', function () {
     var id = $(this).data("id");
 
-    if (confirm("Are you sure you want to delete this post?")) {
+    if (confirm("Are you sure you want to delete this Management Member?")) {
         // Generate route URL dynamically
         var routeTemplate = "{{ route('deleteManegement.delete', ['id' => '__ID__']) }}";
         var deleteUrl = routeTemplate.replace('__ID__', id);
